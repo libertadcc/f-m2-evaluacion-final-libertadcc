f3a994d2c1ccf2253009585a0a6e8eac33590f92
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2019-04-22 10:19:02 +0200

    Fix routes

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,21 +4,24 @@ const url = 'http://api.tvmaze.com/search/shows?q=';
 
 const btn = document.querySelector('.btn');
 const input = document.querySelector('.input');
-
+const results = document.querySelector('.results');
+const item = document.createElement('li');
+item.classList.add('result__serie');
 function search(){
   fetch(`${url}${input.value}`)
     .then(response => response.json())
     .then(data => {
+      results.innerHTML='';
       for(let i = 0; i<data.length; i++){
-        const results = document.querySelector('.results');
-        const item = document.createElement('li');
+        
+        
         const title = document.createElement('h2');
         const img = document.createElement('img');
-        const serie = data[i].show;
 
+        const serie = data[i].show;
         const contName = document.createTextNode(`${serie.name}`);
 
-        const printImg =() =>{
+        const printImg = () =>{
           if(serie.image === null){
             img.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
             item.appendChild(img);
@@ -28,21 +31,23 @@ function search(){
           }
         };
         printImg();
-        // img.setAttribute('src', `${serie.image.medium}`);
-        console.log(serie);
-        //console.log(serie.image.medium);
+        
         title.appendChild(contName);
         item.appendChild(title);
 
         img.setAttribute('alt', `Portada de ${serie.name}`);
         item.appendChild(img);
         results.appendChild(item);
-
       }
-
-      // results.innerHTML='';
-
     });
 }
 
 btn.addEventListener('click', search);
+
+const serieFav = document.querySelectorAll('.result__serie');
+for
+function fav(){
+  console.log('ey');
+
+}
+serieFav.addEventListener('click', fav);
