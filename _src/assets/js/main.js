6c567ac3dbe63361012326ce49d240450acc93d8
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2019-04-22 13:11:45 +0200

    Fav

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,37 +5,38 @@ const url = 'http://api.tvmaze.com/search/shows?q=';
 const btn = document.querySelector('.btn');
 const input = document.querySelector('.input');
 const results = document.querySelector('.results');
-const item = document.createElement('li');
-item.classList.add('result__serie');
+
+// <------  Search series
 function search(){
   fetch(`${url}${input.value}`)
     .then(response => response.json())
     .then(data => {
       results.innerHTML='';
       for(let i = 0; i<data.length; i++){
-        
-        
+        const item = document.createElement('li');
+        item.classList.add('result__serie');
+
         const title = document.createElement('h2');
+        title.classList.add('result__title');
         const img = document.createElement('img');
+        img.classList.add('result__img');
 
         const serie = data[i].show;
         const contName = document.createTextNode(`${serie.name}`);
 
-        const printImg = () =>{
-          if(serie.image === null){
-            img.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
-            item.appendChild(img);
-          } else {
-            img.setAttribute('src', `${serie.image.medium}`);
-            item.appendChild(img);
-          }
-        };
-        printImg();
-        
+        if(serie.image === null){
+          img.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
+          item.appendChild(img);
+          img.setAttribute('alt', `Portada de ${serie.name} no disponible`);
+        } else {
+          img.setAttribute('src', `${serie.image.medium}`);
+          item.appendChild(img);
+          img.setAttribute('alt', `Portada de ${serie.name}`);
+        }
+        item.addEventListener('click', fav);
         title.appendChild(contName);
         item.appendChild(title);
 
-        img.setAttribute('alt', `Portada de ${serie.name}`);
         item.appendChild(img);
         results.appendChild(item);
       }
@@ -44,10 +45,29 @@ function search(){
 
 btn.addEventListener('click', search);
 
-const serieFav = document.querySelectorAll('.result__serie');
-for
+//<---- Favourite series
 function fav(){
-  console.log('ey');
+  const allSeries = document.querySelectorAll('.result__serie');
+  for(let each of allSeries){
+    //Selecciona fav y le añade la clase
+    const favSerie = event.currentTarget;
+    favSerie.classList.add('fav__serie');
+    console.log(favSerie);
+    const favTitle = event.currentTarget.firstChild;
+    favTitle.classList.add('fav__title');
+    console.log(favTitle);
+    const favImg = event.currentTarget.lastChild;
+    favImg.classList.add('fav__img');
 
+    const favs =[
+      
+    ];
+
+    const favResult = document.querySelector('.fav__result');
+    
+
+    //Crear un array con los títulos seleccionados y meterlo en favResult
+  }
 }
-serieFav.addEventListener('click', fav);
+
+//<----- My favourite series
