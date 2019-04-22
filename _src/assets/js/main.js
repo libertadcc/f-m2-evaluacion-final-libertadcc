0d3167ecfbdc10aec7d60f4aec3d3552bd03708d
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2019-04-22 13:54:57 +0200

    Some steps in fav list

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,6 +6,8 @@ const btn = document.querySelector('.btn');
 const input = document.querySelector('.input');
 const results = document.querySelector('.results');
 
+const arrFav = []; //EN este array tenemos que meter los objetos
+
 // <------  Search series
 function search(){
   fetch(`${url}${input.value}`)
@@ -33,7 +35,11 @@ function search(){
           item.appendChild(img);
           img.setAttribute('alt', `Portada de ${serie.name}`);
         }
+
+
+
         item.addEventListener('click', fav);
+
         title.appendChild(contName);
         item.appendChild(title);
 
@@ -46,28 +52,32 @@ function search(){
 btn.addEventListener('click', search);
 
 //<---- Favourite series
+//A la funcion fav hay que meterle un parámetro que serán los objetos de cuando se selecciona una peli
 function fav(){
   const allSeries = document.querySelectorAll('.result__serie');
   for(let each of allSeries){
-    //Selecciona fav y le añade la clase
+  //Selecciona fav y le añade la clase
     const favSerie = event.currentTarget;
     favSerie.classList.add('fav__serie');
-    console.log(favSerie);
+    //console.log(favSerie);
+
+
+    //Añadir las clases fav__title / fav__img
     const favTitle = event.currentTarget.firstChild;
     favTitle.classList.add('fav__title');
-    console.log(favTitle);
+    //console.log(favTitle);
     const favImg = event.currentTarget.lastChild;
     favImg.classList.add('fav__img');
 
-    const favs =[
-      
-    ];
+    //Hacemos objeto
+    console.log(favTitle.innerHTML);
+    const obj = {title: `${favTitle.innerHTML}`,
+      photo: `${favImg.ge}`};//de aquí coger el atributo src
+    console.log('!!!!!', obj);
 
     const favResult = document.querySelector('.fav__result');
-    
 
-    //Crear un array con los títulos seleccionados y meterlo en favResult
+  //Crear un array con los títulos seleccionados y meterlo en favResult
+  //}
   }
 }
-
-//<----- My favourite series
